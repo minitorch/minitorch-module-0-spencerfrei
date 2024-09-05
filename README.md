dc10908522444518363c89654367bbdfebf4dc96
spencerfrei <frei@berkeley.edu>
2024-09-05 10:39:25 -0700

    task 0_5: dataset docstrings and decision boundary in readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=15713478&assignment_repo_type=AssignmentRepo)
+
 # MiniTorch Module 0
 
 <img src="https://minitorch.github.io/minitorch.svg" width="50%">
@@ -6,3 +7,10 @@
 * Docs: https://minitorch.github.io/
 
 * Overview: https://minitorch.github.io/module0/module0/
+
+<p align="center">
+<img src="decision_boundary.png" width=50%">
+</p>
+
+* Decision boundary for a simple linear classifier with weights $w=(-0.71, 0.02)$ and bias $b=0.32$)
+
--- minitorch/datasets.py
+++ minitorch/datasets.py
@@ -5,6 +5,7 @@ from typing import List, Tuple
 
 
 def make_pts(N):
+    """Creates a list of N (x,y) pairs, where x and y are uniform(0,1)."""
     X = []
     for i in range(N):
         x_1 = random.random()
@@ -15,12 +16,19 @@ def make_pts(N):
 
 @dataclass
 class Graph:
+    """Class for generating plots.
+    
+    Attributes:
+        N:  number of points
+        X:  list of (x,y) pairs of floats to be plotted
+        y:  list of integer labels for each (x,y) pair"""
     N: int
     X: List[Tuple[float, float]]
     y: List[int]
 
 
 def simple(N):
+    """Plots N points where y=1 iff first coordinate is <0.5."""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -30,6 +38,7 @@ def simple(N):
 
 
 def diag(N):
+    """Plots N points where y=1 iff sum of coordinates is < 0.5."""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -39,6 +48,7 @@ def diag(N):
 
 
 def split(N):
+    """Plots N points where y=1 iff x_1 < 0.2 or x_1 > 0.8"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -48,6 +58,7 @@ def split(N):
 
 
 def xor(N):
+    """Plots N points for xor function: y=1 iff (x_1<0.5 and x_2>0.5) OR (x_1 > 0.5 and x_2 < 0.5)"""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -57,6 +68,7 @@ def xor(N):
 
 
 def circle(N):
+    """Plots N points where decision boundary is a circle centered at (0.5, 0.5), radius sqrt(0.1); y=1 outside."""
     X = make_pts(N)
     y = []
     for x_1, x_2 in X:
@@ -67,6 +79,7 @@ def circle(N):
 
 
 def spiral(N):
+    """Plots a spiral decision boundary."""
     def x(t):
         return t * math.cos(t) / 20.0
 
